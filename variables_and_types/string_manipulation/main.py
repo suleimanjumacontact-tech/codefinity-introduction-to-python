87f3dd4fc3a758be05370376dc0322a1a46bd905
suleimanjumacontact-tech <>
2026-03-18 18:14:51 +0000

    String Manipulation

---
+++ variables_and_types/string_manipulation/main.py
@@ -0,0 +1,5 @@
+grocery_items = "milk cheese bread apples oranges chicken"
+dairy1 = grocery_items[0:4]
+dairy2 = grocery_items[5:11]
+dairy3 = grocery_items[12:17]
+concatenation = dairy1 + " " + dairy2 + " " + dairy3
